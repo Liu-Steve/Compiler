8fbab7264d392cd5eb2f5337c029ed4a93ba8c50
Liu-Steve <steve_liu@whu.edu.cn>
2022-10-25 18:21:13 +0800

    验收完毕

--- Work1/grammar_type.py
+++ Work1/grammar_type.py
@@ -14,7 +14,14 @@ class Grammar:
         self.vn = set(vn)
         self.vt = set([])
         self.type = 3
+        # 格式化后的所有产生式规则
         self.pro = []
+        # 是否包含符合A->e的文法
+        # 仅对2型及以上有效
+        self.ex = False
+        # 3: 左线性&右线性 2: 右线性 1: 左线性 0: 非线性
+        # 仅对2型及以上有效
+        self.linear = 3
         
         if self.start not in self.vn:
             raise Grammar.StartSymbolException()
@@ -28,12 +35,16 @@ class Grammar:
         for ch in (input_str + output_str):
             if ch not in self.vn:
                 self.vt.add(ch)
-            else:
+        for ch in input_str:
+            if ch in self.vn:
                 has_vn = True
         
         # 左侧缺非终结符号, 不是正确的文法
         if not has_vn:
             return -1
+        # 符合A->e的文法
+        if input_len == 1 and output_len == 0:
+            return -2
 
         # type 0
         if input_len > output_len:
@@ -41,8 +52,42 @@ class Grammar:
         # type 1
         if input_len > 1:
             return 1
-        if input_str not in self.vn:
-            return 1
+        
+        # 左右线性判断
+        l_linear = True
+        r_linear = True
+        for ch in output_str[:-1]:
+            if ch in self.vn:
+                l_linear = False
+                break
+        if output_str[-1] not in self.vn:
+            l_linear = False
+        for ch in output_str[1:]:
+            if ch in self.vn:
+                r_linear = False
+                break
+        if output_str[0] not in self.vn:
+            r_linear = False
+        if output_len == 1:
+            l_linear = True
+            r_linear = True
+        
+        if self.linear == 3:
+            if not l_linear and not r_linear:
+                self.linear = 0
+            elif l_linear and r_linear:
+                self.linear = 3
+            elif l_linear:
+                self.linear = 2
+            elif r_linear:
+                self.linear = 1
+        elif self.linear == 2:
+            if not l_linear:
+                self.linear = 0
+        elif self.linear == 1:
+            if not r_linear:
+                self.linear = 0
+        
         # type 2
         if output_len > 2:
             return 2
@@ -59,9 +104,17 @@ class Grammar:
     def add_rules(self, input_str, output_strs):
         for output_str in output_strs:
             t = self.get_rules_type(input_str, output_str)
-            if t < self.type:
+            if t == -2:
+                self.ex = True # 拓展型
+            elif t < self.type:
                 self.type = t
+        # 1型没有拓展型
+        if self.ex and self.type == 1:
+            self.type = 0
                 
+        # 补回'_'
+        output_strs = [(ele if ele.__len__() > 0 else '_') \
+            for ele in output_strs]
         out = '|'.join(output_strs)
         rule = ''.join([input_str, ' ::= ', out])
         self.pro.append(rule)
@@ -122,13 +175,15 @@ def parse_rule(rule):
     sp = rule.find('::=')
     input_str = rule[:sp].strip()
     output_strs = rule[sp + 3:].split('|')
-    output_strs = [ele.strip() for ele in output_strs]
+    output_strs = [ele.strip().replace('_', '') \
+        for ele in output_strs]
     
     # 不是字母数字
     if not input_str.isalnum():
         return False, '', []
     for output_str in output_strs:
-        if not output_str.isalnum():
+        if not output_str.isalnum() \
+            and output_str.__len__() > 0:
             return False, '', []
         
     return True, input_str, output_strs
@@ -185,14 +240,33 @@ def main():
         line += 1
         
     # 输出结果
-    print(f'文法 {grammar.name}[{grammar.start}] = ({grammar.vn}, {grammar.vt}, Pro, {grammar.start})')
+    print(f'文法 {grammar.name}[{grammar.start}] = ' + \
+          f'({grammar.vn}, {grammar.vt}, Pro, {grammar.start})')
     print('Pro:', end='')
     for p in grammar.pro:
         print(f'\t{p}')
+    # 拓展型判断
+    if grammar.ex and grammar.type >= 2:
+        ex = "拓展"
+    else:
+        ex = ""
+    # 左右线性都算3型
+    if grammar.type >= 2 and grammar.linear > 0:
+        grammar.type = 3
     if grammar.type == -1:
         print(f'该文法左侧缺少非终结符号, 不是 Chomsky 文法')
     else:
-        print(f'该文法是 Chomsky{grammar.type} 型文法')
+        print(f'该文法是{ex} Chomsky{grammar.type} 型文法')
+    # 线性文法
+    if grammar.type >= 2:
+        if grammar.linear == 3:
+            print('该文法既是左线性文法又是右线性文法')
+        elif grammar.linear == 1:
+            print('该文法是左线性文法')
+        elif grammar.linear == 2:
+            print('该文法是右线性文法')
+        else:
+            print('该文法不是线性文法')
     
 
 if __name__ == '__main__':
